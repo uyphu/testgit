f59232c3f0ba52277cd5b68a6c7fcf23bc0735bc
phultu <phultu@fsoft.com.vn>
2017-06-09 14:38:51 -0700

    test 2

---
+++ src/main/java/com/test/git/com/test/git/Test2.java
@@ -0,0 +1,22 @@
+/**
+ * Test2.java
+ * Copyright (c) yyyy DIRECTV, Inc
+ * An Unpublished Work.  All Rights Reserved.
+ *
+ * DIRECTV PROPRIETARY:  The information contained in or disclosed by this
+ * document is considered proprietary by DIRECTV, Inc.  This document and/or
+ * the information contained therein shall not be duplicated nor disclosed in
+ * whole or in part without the specific written permission of DIRECTV, Inc.
+ * phultu
+ * Jun 9, 2017
+ */
+package com.test.git.com.test.git;
+
+/**
+ * @author phultu
+ * @date Jun 9, 2017
+ *
+ */
+public class Test2 {
+
+}
